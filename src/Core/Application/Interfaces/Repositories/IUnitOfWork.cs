c04db36cac72e376e15a3a58c27bcf1d024e4e8a
JohnZhaoXiaoHu <39523510+JohnZhaoXiaoHu@users.noreply.github.com>
2023-07-31 11:38:35 +0800

    Initial commit

---
+++ src/Core/Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IUnitOfWork
+    {
+        Task SaveChangesAsync();
+        void SaveChanges();
+
+        IDbTransaction BeginTransaction();
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+using Persistence.Context;
+using System.Data;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CAContext _context;
+
+        public UnitOfWork(CAContext context)
+        {
+            _context = context;
+        }
+
+        public IDbTransaction BeginTransaction()
+        {
+            var transaction = _context.Database.BeginTransaction();
+            return transaction.GetDbTransaction();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
